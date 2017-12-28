d6f64197e2f6a268308248b79796854c4094ca4e
Caleb-Tan <calebtan23@gmail.com>
2017-12-28 15:19:54 -0800

    changed host

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ if __name__ == "__main__":
     sched.add_cron_job(lambda: post_tasks('Business', 'visible'), hour=7)
 
     sched.start()
-    app.run(debug=True, port=7000)
+    app.run(debug=True, host='0.0.0.0', port=7000)
 
     
     
